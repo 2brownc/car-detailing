22aeb37007bcd4a3576f686c5abf9e7b91844c3d
Brown <2brownit@gmail.com>
2023-01-30 19:36:19 +0530

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-function Footer() {
+import { AiOutlinePhone } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai';
+import { AiOutlineFacebook } from 'react-icons/ai';
+import { FiTwitter } from 'react-icons/fi';
+
+import type { slidesType } from "../types/carousel";
+import { cardsData } from '../../data/cards';
+
+
+function Footer2() {
   return (
     <div
       className="
@@ -57,6 +67,163 @@ function Footer() {
       </div>
     </div>
 
+  );
+}
+
+function Footer() {
+  const router = useRouter();
+  const servicesData = cardsData.filter(cardData => cardData.title !== 'Contact');
+  const goHome = () => {
+    router.push('/');
+  };
+  const goTo = (link: string) => {
+    router.push(link);
+  };
+  return (
+    <div className="
+        flex md:flex-row flex-col
+        justify-evenly
+        bg-black
+        text-white
+        gap-5
+        p-10
+      ">
+      <div className="self-center">
+        <div
+          className="
+          basis-auto grow
+          py-4
+          text-center
+          font-['Passion_One']
+          font-bold
+          text-3xl
+          md:text-5xl
+          tracking-wider
+          hover:cursor-pointer
+          hover:scale-110
+          hover:text-orange-500
+          transition-all
+          duration-500
+        "
+          onClick={goHome}
+        >
+          PK DETAILING
+        </div>
+      </div>
+      <div className="
+        ">
+        <div className="
+            capitalize
+            font-bold
+            tracking-wide
+            text-xl md:text-2xl
+            mb-1
+          ">
+          Services
+        </div>
+        <ul>
+          {
+            servicesData.map(service => (
+              <li className="
+                transition-all
+                duration-500
+                p-1
+                hover:bg-orange-600
+              ">
+                <div onClick={() => goTo(service.link)}>
+                  {service.title}
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="
+      ">
+        <div className="
+          capitalize
+          font-bold
+          tracking-wide
+          text-xl md:text-2xl
+          mb-1
+        ">
+          Contact
+        </div>
+        <ul>
+          <li className="
+                transition-all
+                duration-500
+                p-1
+                hover:bg-orange-600
+              ">
+            <div className="flex flex-row items-center gap-1">
+              <AiOutlinePhone />
+              <span>+32 0473 72 01 34</span>
+            </div>
+          </li>
+          <li className="
+                transition-all
+                duration-500
+                p-1
+                hover:bg-orange-600
+              ">
+            <div className="flex flex-row items-center gap-1">
+              <AiOutlineMail />
+              <a href="mailto:pkdetailing@gmail.com">pkdetailing@gmail.com</a>
+            </div>
+          </li>
+          <li className="
+                transition-all
+                duration-500
+                p-1
+                hover:bg-orange-600
+              ">
+            <div className="flex flex-row items-center gap-1">
+              <AiOutlineFacebook />
+              <a href="https://facebook.com/pkdetialing">
+                pkdetailing
+              </a>
+            </div>
+          </li>
+          <li className="
+                transition-all
+                duration-500
+                p-1
+                hover:bg-orange-600
+              ">
+            <div className="flex flex-row items-center gap-1">
+              <FiTwitter />
+              <a href="https://twitter.com/@pkdetailing">
+                @pkdetailing
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="
+      ">
+        <div className="
+          capitalize
+          font-bold
+          tracking-wide
+          text-xl md:text-2xl
+          mb-1
+        ">
+          Address
+        </div>
+        <div>
+          <address className="flex flex-col">
+            <span>Tiegemberg 441,</span>
+            <span>Tongrinne,</span>
+            <span>Namur,</span>
+            <span>Belgium - 5140</span>
+          </address>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
