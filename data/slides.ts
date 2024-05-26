daf008de4f1a1a7d48e44dc9de8178bf37c971ca
brown <86018533+2brownc@users.noreply.github.com>
2024-05-26 21:03:41 +0530

    prune dead links

--- data/slides.ts
+++ data/slides.ts
@@ -1,37 +1,30 @@
 import type { slidesType } from "../types/carousel";
 
-import interior from '../public/services/interior.jpg';
-import exterior from '../public/services/exterior.jpg';
-import headlight from '../public/services/headlight.jpg';
-import ceramicCoating from '../public/services/ceramic-coating.jpg';
-import polishing from '../public/services/polishing.jpg';
-
+import interior from "../public/services/interior.jpg";
+import exterior from "../public/services/exterior.jpg";
+import headlight from "../public/services/headlight.jpg";
+import polishing from "../public/services/polishing.jpg";
 
 const slidesData: slidesType = [
   {
-    text: 'Exterior Detailing',
-    link: '/exterior',
-    image: exterior
+    text: "Exterior Detailing",
+    link: "/exterior",
+    image: exterior,
   },
   {
-    text: 'Interior Detailing',
-    link: '/interior',
-    image: interior
+    text: "Interior Detailing",
+    link: "/interior",
+    image: interior,
   },
   {
-    text: 'Headlight Polishing',
-    link: '/headlight',
-    image: headlight
+    text: "Headlight Polishing",
+    link: "/headlight",
+    image: headlight,
   },
   {
-    text: 'Ceramic Coating',
-    link: '/ceramic-coating',
-    image: ceramicCoating
-  },
-  {
-    text: 'Polishing',
-    link: '/polishing',
-    image: polishing
+    text: "Polishing",
+    link: "/polishing",
+    image: polishing,
   },
 ];
 
